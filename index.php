d53963edccbe956d86d57617bcfbbe7ab65ca2f3
zackad <zackad.dev@gmail.com>
2020-02-01 17:03:41 +0700

    [style] fix code style with php csfixer

--- index.php
+++ index.php
@@ -7,25 +7,25 @@ use Slim\Factory\AppFactory;
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
-    $file = urldecode(__DIR__ . $url['path']);
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    $file = urldecode(__DIR__.$url['path']);
     if (is_file($file)) {
         return false;
     }
 }
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/[{route:.+}]', function(Request $request, Response $response, $args) {
-    $targetDir = $_SERVER['REQUEST_URI'] === '/' ? '' : urldecode($_SERVER['REQUEST_URI']);
+$app->get('/[{route:.+}]', function (Request $request, Response $response, $args) {
+    $targetDir = '/' === $_SERVER['REQUEST_URI'] ? '' : urldecode($_SERVER['REQUEST_URI']);
 
-    $entries = preg_grep('/^([^.])/', scandir(__DIR__ . $targetDir));
+    $entries = preg_grep('/^([^.])/', scandir(__DIR__.$targetDir));
     natsort($entries);
 
-    foreach($entries as $entry) {
-        $uri = $targetDir . '/' . $entry;
+    foreach ($entries as $entry) {
+        $uri = $targetDir.'/'.$entry;
         $link = sprintf('<a href="%s">%s</a></br>', $uri, $entry);
         echo $link;
     }
@@ -34,4 +34,3 @@ $app->get('/[{route:.+}]', function(Request $request, Response $response, $args)
 });
 
 $app->run();
-
