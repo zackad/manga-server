cca11e25d0b1210c278c138ef18311d720fd90e4
zackad <zackad.dev@gmail.com>
2020-02-04 18:58:41 +0700

    [feat] pass categorized entry list as props

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,10 @@
 import { h, render } from 'preact'
 
-function App () {
+function App(props) {
   return <div>Manga Server App</div>
 }
 
-render(<App/>, document.getElementById('preact_container'))
+const directories = ENTRIES_DATA.filter(entry => entry.isDirectory)
+const files = ENTRIES_DATA.filter(entry => !entry.isDirectory)
+
+render(<App directories={directories} files={files} />, document.getElementById('preact_container'))
