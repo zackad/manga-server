7c461dbca4e9c3b957d58061c538665845b3ba41
zackad <zackad.dev@gmail.com>
2020-02-07 21:23:30 +0700

    [feat] add state to toggle mode

--- assets/App.jsx
+++ assets/App.jsx
@@ -1,14 +1,19 @@
 import { h } from 'preact'
+import { useState } from 'preact/hooks'
 import { Breadcrumbs } from './Components/Breadcrumbs'
 import { Listing } from './Components/Listing'
 import { Reader } from './Components/Reader'
 
 function App(props) {
+  const [mode, setMode] = useState('list')
+
+  const list = <Listing files={props.files} directories={props.directories} />
+  const reader = <Reader images={props.files} />
+
   return (
     <div>
       <Breadcrumbs />
-      <Listing files={props.files} directories={props.directories} />
-      <Reader images={props.files} />
+      {mode === 'list' ? list : reader}
     </div>
   )
 }
