395c354f26fb856766b9433f0e2d32fce944126a
zackad <zackad.dev@gmail.com>
2020-02-07 21:54:08 +0700

    [feat] add postcss loader to proccess tailwind

--- assets/App.jsx
+++ assets/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'preact/hooks'
 import { Breadcrumbs } from './Components/Breadcrumbs'
 import { Listing } from './Components/Listing'
 import { Reader } from './Components/Reader'
-import './css/tailwind.css'
+import './css/tailwind.src.css'
 
 function App(props) {
   const [mode, setMode] = useState('list')
