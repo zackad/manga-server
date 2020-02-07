5d609043c2d4f0ae575d1fa90e71f06386329bd7
zackad <zackad.dev@gmail.com>
2020-02-07 21:38:33 +0700

    [feat] add handler to toggle mode

--- assets/App.jsx
+++ assets/App.jsx
@@ -1,5 +1,5 @@
 import { h } from 'preact'
-import { useState } from 'preact/hooks'
+import { useEffect, useState } from 'preact/hooks'
 import { Breadcrumbs } from './Components/Breadcrumbs'
 import { Listing } from './Components/Listing'
 import { Reader } from './Components/Reader'
@@ -7,8 +7,35 @@ import { Reader } from './Components/Reader'
 function App(props) {
   const [mode, setMode] = useState('list')
 
+  const regexFilter = new RegExp('.jpe?g$|.png$|.gif$|.webp$', 'i')
+  const images = props.files.filter(image => image.uri.match(regexFilter))
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeydown)
+  }, [])
+
+  const handleKeydown = event => {
+    const { keyCode } = event
+
+    switch (keyCode) {
+      // 'Enter/Return' key
+      case 13:
+        if (images.length > 0) {
+          setMode('reader')
+        }
+        break
+      // ';' (Semicolon)
+      case 59:
+        break
+      // '\' key
+      case 220:
+        setMode('list')
+        break
+    }
+  }
+
   const list = <Listing files={props.files} directories={props.directories} />
-  const reader = <Reader images={props.files} />
+  const reader = <Reader images={images} />
 
   return (
     <div>
