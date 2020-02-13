cfa44d4adcc9bf3cc855061208d8af874d428311
zackad <zackad.dev@gmail.com>
2020-02-13 14:42:54 +0700

    [chore] put build artifact into dist directory

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ composer install --no-dev
 composer dump-autoload --optimize
 
 yarn build
-zip -r manga-server.$(git describe --abbrev=0).zip static vendor index.php template.html.twig
+mkdir dist
+zip -r dist/manga-server.$(git describe --abbrev=0).zip static vendor index.php template.html.twig
