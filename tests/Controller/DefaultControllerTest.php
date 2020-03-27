a086f02acbeeb19658c4c5b9eaa789363d3510be
zackad <zackad.dev@gmail.com>
2020-03-27 13:34:18 +0700

    [Test] add more image stub to test

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -6,11 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testLoadJpegImage()
+    /**
+     * @dataProvider imageProvider
+     */
+    public function testLoadImage($image)
     {
         $client = static::createClient();
-        $client->request('GET', '/image.jpeg');
+        $client->request('GET', $image);
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('Expires', (new \DateTime('+1 week'))->format(DATE_RFC7231));
+    }
+
+    public function imageProvider()
+    {
+        return [
+            ['/image.jpg'],
+            ['/image.jpeg'],
+            ['/image.png'],
+            ['/image.webp'],
+        ];
     }
 }
