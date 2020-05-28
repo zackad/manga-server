e554251bc26fa34d5ef0f824a68b6a0870da9ad8
zackad <zackad.dev@gmail.com>
2020-05-28 13:11:49 +0700

    [ui] add bottom border to list wrapper

--- assets/Components/List/Wrapper.jsx
+++ assets/Components/List/Wrapper.jsx
@@ -3,7 +3,7 @@ import { h } from 'preact'
 export function Wrapper({ children }) {
   return (
     <div className='w-full md:w-1/2 lg:w-1/3 px-2 pt-2 mt-2'>
-      <div className='flex'>{children}</div>
+      <div className='flex border-b border-gray-600'>{children}</div>
     </div>
   )
 }
