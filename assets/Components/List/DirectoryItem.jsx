75909d167fc41f15a4693380b8661c80e3fcdf0d
zackad <zackad.dev@gmail.com>
2020-05-29 12:06:47 +0700

    [ui] reduce directory icon size

--- assets/Components/List/DirectoryItem.jsx
+++ assets/Components/List/DirectoryItem.jsx
@@ -6,10 +6,10 @@ import { IconDirectory } from '../IconDirectory'
 export function DirectoryItem({ label, uri }) {
   return (
     <Wrapper>
-      <div className='w-1/4 h-16 overflow-hidden'>
+      <div className='w-12 h-6 overflow-hidden'>
         <IconDirectory />
       </div>
-      <div className='w-3/4 h-16 overflow-hidden'>
+      <div className='w-full h-6 overflow-hidden'>
         <h1>
           <a className='inline-flex items-start w-full' href={uri}>
             <span>{label}</span>
