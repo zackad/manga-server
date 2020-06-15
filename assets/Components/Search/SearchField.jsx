582824ccdbe0f5a48159436d0948cc452ae9a707
zackad <zackad.dev@gmail.com>
2020-06-15 19:24:30 +0700

    [ui] remove searcg button

--- assets/Components/Search/SearchField.jsx
+++ assets/Components/Search/SearchField.jsx
@@ -4,10 +4,7 @@ export function SeacrhField() {
   return (
     <span className='mr-2'>
       <form action='/search' className='flex'>
-        <input type='text' name='q' className='mr-2 px-1 h-8 text-sm text-gray-900' />
-        <button type='submit' className='p-1 h-8 text-xs border'>
-          Search
-        </button>
+        <input type='text' name='q' className='px-1 h-8 text-sm text-gray-900' />
       </form>
     </span>
   )
