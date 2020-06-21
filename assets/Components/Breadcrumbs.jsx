1188a70c0fd172ff240e2159435ecd147b934fb0
zackad <zackad.dev@gmail.com>
2020-06-21 17:17:16 +0700

    Revert "[refactor] remove Breadcrumbs component"
    This reverts commit fad6e6212cb6479a0ed5d09da054b3c4208ef610.
    
    Breadcrumbs component still be needed, only placement will be
    different.

---
+++ assets/Components/Breadcrumbs.jsx
@@ -0,0 +1,39 @@
+import { h } from 'preact'
+
+import { LabelItem } from './LabelItem.jsx'
+import { LinkItem } from './LinkItem.jsx'
+
+function Breadcrumbs(props) {
+  const location = decodeURIComponent(document.location.pathname)
+  let items = location.split('/').filter(item => item !== '')
+
+  let breadcrumbs = [{ path: '/', title: 'Root' }]
+  let link = ''
+  for (let item of items) {
+    link += '/' + item
+    breadcrumbs.push({
+      path: encodeURIComponent(link.replace(/^\/+|\/+$/g, '')),
+      title: decodeURIComponent(item),
+    })
+  }
+
+  breadcrumbs.map((item, i, arr) => {
+    if (arr.length - 1 === i) {
+      item.element = <LabelItem title={item.title} />
+    } else {
+      item.element = <LinkItem path={item.path} title={item.title} />
+    }
+    return item
+  })
+
+  return (
+    <div className={`p-2 border-b bg-gray-800 flex w-full`}>
+      <div className='flex overflow-x-scroll'>{breadcrumbs.map(item => item.element)}</div>
+      <span className={`flex-grow`} />
+      {props.toggleSetting}
+      {props.toggleReader}
+    </div>
+  )
+}
+
+export { Breadcrumbs }
