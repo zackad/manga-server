8fa3ac44e0fa607471010549842308412e471856
zackad <zackad.dev@gmail.com>
2020-06-21 17:16:53 +0700

    [feat] add home button for navigation helper

--- assets/Components/AppBar.jsx
+++ assets/Components/AppBar.jsx
@@ -2,8 +2,9 @@ import { h } from 'preact'
 import { useContext } from 'preact/hooks'
 
 import { ButtonIcon } from './Button/ButtonIcon'
-import { IconCog } from './Icon/IconCog'
 import { IconBookOpen } from './Icon/IconBookOpen'
+import { IconCog } from './Icon/IconCog'
+import { IconHome } from './Icon/IconHome'
 import { AppContext } from '../App'
 
 export function AppBar() {
@@ -11,6 +12,11 @@ export function AppBar() {
 
   return (
     <div className='flex py-1 bg-gray-800 border-b'>
+      <a href='/'>
+        <ButtonIcon>
+          <IconHome />
+        </ButtonIcon>
+      </a>
       <div className='flex-grow'></div>
       <ButtonIcon onClick={toggleSettingDialog}>
         <IconCog />
