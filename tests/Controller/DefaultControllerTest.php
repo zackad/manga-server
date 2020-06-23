f1d56a4f29ba2bdda94b97db05bb348e8234f009
zackad <zackad.dev@gmail.com>
2020-06-23 19:21:42 +0700

    [test] add test case for default controller

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -21,6 +21,22 @@ class DefaultControllerTest extends WebTestCase
         $this->assertLessThanOrEqual((new \DateTime('+1 week'))->getTimestamp(), $expires);
     }
 
+    public function testAccessNonExistingDirectory()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/non-existing-directory');
+
+        $this->assertResponseStatusCodeSame(404);
+    }
+
+    public function testAccessHomepage()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+    }
+
     public function imageProvider()
     {
         return [
