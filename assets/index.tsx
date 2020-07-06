59445761fa5be2cdf2198fc8e15e97efc829897f
zackad <zackad.dev@gmail.com>
2020-07-06 15:16:52 +0700

    [config] add tsconfig and rename entrypoint

---
+++ assets/index.tsx
@@ -0,0 +1,10 @@
+import React from 'preact/compat'
+import { render } from 'preact'
+
+import { App } from './App'
+
+const directories = ENTRIES_DATA.filter((entry) => entry.type === 'directory')
+const archive = ENTRIES_DATA.filter((entry) => entry.type === 'archive')
+const files = ENTRIES_DATA.filter((entry) => entry.type === 'file')
+
+render(<App directories={directories} files={files} archive={archive} />, document.getElementById('preact_container'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const Encore = require('@symfony/webpack-encore')
 
-Encore.addEntry('app', './assets/index.js')
+Encore.addEntry('app', './assets/index.tsx')
   .setOutputPath('public/build/')
   .setPublicPath('/build')
   .disableSingleRuntimeChunk()
