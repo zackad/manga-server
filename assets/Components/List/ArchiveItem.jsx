46c5979eabd9b7174e31fe93b3a25d91bba08892
zackad <zackad.dev@gmail.com>
2020-09-26 17:08:29 +0700

    feat: make thumbnail cover clickable

--- assets/Components/List/ArchiveItem.jsx
+++ assets/Components/List/ArchiveItem.jsx
@@ -7,7 +7,7 @@ import { ListLabel } from './ListLabel'
 export function ArchiveItem({ cover, label, uri }) {
   return (
     <ArchiveWrapper>
-      <div className='w-1/2 md:w-full md:h-80 overflow-hidden'>{cover && <Thumbnail image={cover} />}</div>
+      <div className='w-1/2 md:w-full md:h-80 overflow-hidden'>{cover && <Thumbnail image={cover} uri={uri} />}</div>
       <div className='w-1/2 md:w-full md:h-12 overflow-hidden'>
         <ListLabel uri={uri} label={label} />
       </div>
