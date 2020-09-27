95f1b798563c991753d5eb15104a968d7b4db70d
zackad <zackad.dev@gmail.com>
2020-09-27 11:54:42 +0700

    fix: declare button icon component props

--- assets/Components/Button/ButtonIcon.tsx
+++ assets/Components/Button/ButtonIcon.tsx
@@ -1,6 +1,12 @@
-import { h } from 'preact'
+import { h, JSX } from 'preact'
 
-export function ButtonIcon({ onClick, children, margin }) {
+type ButtonIconProps = {
+  margin?: string
+  onClick?: any
+  children?: JSX.Element
+}
+
+export function ButtonIcon({ onClick, children, margin }: ButtonIconProps) {
   return (
     <button className={`inline-block ${margin} p-1 w-8 h-8`} onClick={onClick}>
       {children}
