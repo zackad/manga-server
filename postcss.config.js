33510d16e6f6ae7d054bf028aa1e4451fc192f84
zackad <zackad.dev@gmail.com>
2021-01-03 15:15:08 +0700

    fix: add tsx into content resolver on purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,12 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
     './assets/**/*.jsx',
+    './assets/**/*.tsx',
     // etc.
   ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
 
 module.exports = {
