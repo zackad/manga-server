c567502b190db23851301b428e3691dbe2110b00
zackad <zackad.dev@gmail.com>
2021-01-03 14:56:31 +0700

    feat: handle redirection when encountering next query string

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Service\DirectoryListing;
+use App\Service\NextChapterResolver;
 use App\Service\PathTool;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\File\Stream;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,10 +18,16 @@ class DefaultController extends AbstractController
      * @Route("/", name="home", methods={"GET"})
      * @Route("/{default}", name="default", methods={"GET"}, requirements={"default"="^(?!build).+"})
      */
-    public function index(DirectoryListing $listing, PathTool $pathTool): Response
+    public function index(DirectoryListing $listing, PathTool $pathTool, Request $request, NextChapterResolver $resolver): Response
     {
         $uriPrefix = $pathTool->getPrefix();
         $target = $pathTool->getTarget();
+
+        $nextPage = '' === $request->query->get('next');
+
+        if ($nextPage) {
+            return $this->redirect($resolver->resolve());
+        }
 
         if (is_file($target)) {
             $stream = new Stream($target);
