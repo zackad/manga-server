77087b3d2e66b2b2ab6638ba5615c17c1dd78db7
zackad <zackad.dev@gmail.com>
2022-02-01 12:34:13 +0700

    fix: add missing next chapter url

--- tests/Twig/AppExtensionTest.php
+++ tests/Twig/AppExtensionTest.php
@@ -6,15 +6,24 @@ namespace App\Tests\Twig;
 
 use App\Twig\AppExtension;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * @covers \App\Twig\AppExtension
  */
 class AppExtensionTest extends TestCase
 {
+    private $requestStack;
+
+    protected function setUp(): void
+    {
+        $this->requestStack = $this->createMock(RequestStack::class);
+    }
+
     public function testMakeSureIsCovered()
     {
-        $extension = new AppExtension();
+        $extension = new AppExtension($this->requestStack);
 
         $this->assertIsArray($extension->getFilters());
         $this->assertIsArray($extension->getFunctions());
@@ -25,9 +34,40 @@ class AppExtensionTest extends TestCase
      */
     public function testIsImage(string $filename, bool $result)
     {
-        $extension = new AppExtension();
+        $extension = new AppExtension($this->requestStack);
 
         $this->assertEquals($result, $extension->isImage($filename));
+    }
+
+    /**
+     * @dataProvider uriProvider
+     */
+    public function testGetTitleFromUri(string $input, ?string $output)
+    {
+        $mainRequest = $this->createMock(Request::class);
+        $mainRequest->method('getRequestUri')->willReturn($input);
+        $this->requestStack->method('getMainRequest')->willReturn($mainRequest);
+        $extension = new AppExtension($this->requestStack);
+
+        $this->assertEquals($output, $extension->getTitleFromUri($input));
+    }
+
+    public function testAutowiring()
+    {
+        $requestStack = $this->createMock(RequestStack::class);
+        $extension = new AppExtension($requestStack);
+
+        $this->assertEquals(null, $extension->getTitleFromUri());
+    }
+
+    public function uriProvider(): array
+    {
+        return [
+            ['string', 'string'],
+            ['/string/title', 'title'],
+            ['/string/title/other', 'other'],
+            ['/string/title/other/', 'other'],
+        ];
     }
 
     public function dataProvider(): array
