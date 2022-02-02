9d17d451ae20e4ec39be4ef380dc458c4aa80e04
zackad <zackad.dev@gmail.com>
2022-02-02 08:10:07 +0700

    chore: add profiler bundle as dependency

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
