cf7d9e62c8b4c209097237f586f7b3b304a65382
zackad <zackad.dev@gmail.com>
2023-09-30 09:06:28 +0700

    style: reformat code with prettier

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './css/tailwind.css';
+import './css/tailwind.css'
 
 // start the Stimulus application
-import './bootstrap';
+import './bootstrap'
