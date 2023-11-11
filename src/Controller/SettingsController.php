d246331c12558456772a62c5a89d094fbc97ac00
zackad <zackad.dev@gmail.com>
2023-11-11 07:42:01 +0700

    feat(wip): settings page

---
+++ src/Controller/SettingsController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SettingsController extends AbstractController
+{
+    /**
+     * @Route("/settings", name="app_settings")
+     */
+    public function index(): Response
+    {
+        return $this->render('settings.html.twig', [
+            'controller_name' => 'SettingsController',
+        ]);
+    }
+}
