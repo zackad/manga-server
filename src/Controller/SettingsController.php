e1be49196958e6ac7758682471d1ec19f88a2be7
zackad <zackad.dev@gmail.com>
2024-10-31 06:56:18 +0700

    use attributes

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -6,13 +6,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class SettingsController extends AbstractController
 {
-    /**
-     * @Route("/settings", name="app_settings")
-     */
+    #[Route(path: '/settings', name: 'app_settings')]
     public function index(): Response
     {
         return $this->render('settings.html.twig', [
